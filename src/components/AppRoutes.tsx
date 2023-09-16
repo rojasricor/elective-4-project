03c67fb53f83881f85cef200b5e19c456aa35f6d
Ricardo Rojas <rojasricor@gmail.com>
2023-09-15 20:07:46 -0500

    login template

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -2,18 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import About from '../pages/About';
 import Contacts from '../pages/Contacts';
 import Home from '../pages/Home';
-import Login from '../pages/Login';
-import PageNotFound from '../pages/PageNotFound';
 import Signin from '../pages/Signin';
+import PageNotFound from '../pages/PageNotFound';
+import Signup from '../pages/Signup';
 
 function AppRoutes(): React.ReactNode {
   return (
     <Routes>
       <Route index element={<Home />} />
 
-      <Route path="/login" element={<Login />} />
-
       <Route path="/signin" element={<Signin />} />
+
+      <Route path="/signup" element={<Signup />} />
 
       <Route path="/about" element={<About />} />
 
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,15 @@
+import { Helmet } from 'react-helmet';
+
+function Signup(): React.ReactNode {
+  return (
+    <>
+      <Helmet>
+        <title>Finanzas Educativas | Registrarse</title>
+      </Helmet>
+
+      <div>Signup</div>
+    </>
+  );
+}
+
+export default Signup;
